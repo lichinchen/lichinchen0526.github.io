902fcc548d5ba566c3f64221174b24eef839d9e7
lichinchen <che20014@byui.edu>
2021-01-25 13:50:05 -0500

    initial commit

---
+++ lesson4/js/lesson4.js
@@ -0,0 +1,16 @@
+// Hamburger Button
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+
+function toggleMenuReSize(){
+  document.getElementsByClassName("navigation")[0].classList.remove("responsive");
+}
+
+//Current date
+const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
+document.getElementById('currentday').textContent = new Date().toLocaleDateString('en-US', options);
+
+
+
