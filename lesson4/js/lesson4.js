e962bcaa09858d42ae724389ace2cb863a989b7c
lichinchen <che20014@byui.edu>
2021-01-29 21:03:26 -0500

    update

--- lesson4/js/lesson4.js
+++ lesson4/js/lesson4.js
@@ -1,4 +1,4 @@
-// Hamburger Button
+/*----- Hamburger Button------*/
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
@@ -8,7 +8,10 @@ function toggleMenuRemove(){
   document.getElementsByClassName("navigation")[0].classList.remove("responsive");
 }
 
-//Current date
+/*-----current year-----*/
+let y = new Date();
+document.getElementById("currentYear").innerHTML = y.getFullYear();
+/*-----Current date-----*/
 const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
 document.getElementById('currentday').textContent = new Date().toLocaleDateString('en-US', options);
 
