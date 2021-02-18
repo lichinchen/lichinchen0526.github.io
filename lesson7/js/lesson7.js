8f04be6773fafbba0a0c196832b000d69dba61e4
lichinchen <che20014@byui.edu>
2021-02-18 07:09:55 -0500

    update js

--- lesson7/js/lesson7.js
+++ lesson7/js/lesson7.js
@@ -22,38 +22,3 @@ if (date.getDay()==5){
     document.querySelector(".onlyFri").style.display="block" 
 }
 
-/*-----Current weekday-----*/
-var d = new Date();
-  var weekday = new Array(7);
-        weekday[0] = "Sunday";
-        weekday[1] = "Monday";
-        weekday[2] = "Tuesday";
-        weekday[3] = "Wednesday";
-        weekday[4] = "Thursday";
-        weekday[5] = "Friday";
-        weekday[6] = "Saturday";
-  
-  for (i = 0; i < 29; i++) {
-    document.getElementById("day1").innerHTML = weekday[(d.getDay()+1 + i) % 7];
-    document.getElementById("day2").innerHTML = weekday[(d.getDay()+2 + i) % 7];
-    document.getElementById("day3").innerHTML = weekday[(d.getDay()+3 + i) % 7];
-    document.getElementById("day4").innerHTML = weekday[(d.getDay()+4 + i) % 7];
-    document.getElementById("day5").innerHTML = weekday[(d.getDay()+5 + i) % 7];
-
-  }
-    
-
-
-/*-----WindChill-----*/
-window.onload = function windChill() {
-  let t = parseFloat(document.getElementById('curTemp').textContent);
-  let s = parseFloat(document.getElementById('windSpeed').textContent); 
-  let output = "N/A";
-  if (t<= 50 && s>=3){
-  let f = (35.74 + (0.6215*t)) - (35.75 * (Math.pow(s, 0.16))) + (0.4275*(t*(Math.pow(s , 0.16))));
-  output = Math.round(f);
-  }
-  document.getElementById("windChill").innerHTML = output;
-}
-
-
