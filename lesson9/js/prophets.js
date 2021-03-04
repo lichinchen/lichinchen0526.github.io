fe02799a30d72cbf864696249599927aa6ae9a4c
lichinchen <che2004@byui.edu>
2021-03-04 01:11:31 -0500

    change screen size

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -8,19 +8,19 @@ fetch(requestURL)
 
     for (let i = 0; i < prophets.length; i++ ) {
       let card = document.createElement('section');
-      let name = document.createElement('h2');
+      let fName = document.createElement('h2');
       let bDate = document.createElement('p');
       let bPlace = document.createElement('p');
       let image = document.createElement('img');
 
-      name.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+      fName.textContent = prophets[i].name + ' ' + prophets[i].lastname;
       bDate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
       bPlace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
 
       image.setAttribute('src', prophets[i].imageurl);
       image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' ' + '-' + ' ' + prophets[i].order);
 
-      card.appendChild(name);
+      card.appendChild(fName);
       card.appendChild(bDate);
       card.appendChild(bPlace);
       card.appendChild(image);
